307a9672345ccb69762c6545fe3b8814c97a7b36
Katie Lonsdale <katie952@gmail.com>
2023-01-31 14:49:00 -0700

    add testing for attributes

--- flash_cards/spec/deck_spec.rb
+++ flash_cards/spec/deck_spec.rb
@@ -2,10 +2,17 @@ require './lib/deck'
 require './lib/card'
 
 RSpec.describe Deck do
-  it 'exists' do
-    deck = Deck.new
+  it 'exists and has attributes' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
 
     expect(deck).to be_instance_of(Deck)
+    expect(deck.cards).to eq(cards)
   end
 
   it 'takes an array of cards' do
@@ -30,7 +37,7 @@ RSpec.describe Deck do
     expect(deck.count).to eq 3
   end
 
-  xit 'returns cards in given category' do
+  it 'returns cards in given category' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
