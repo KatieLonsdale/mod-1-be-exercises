d82e215f9f5b2b9ddd71eb9525bf327f15748a27
Katie Lonsdale <katie952@gmail.com>
2023-01-31 14:46:24 -0700

    make count its own method

--- flash_cards/lib/deck.rb
+++ flash_cards/lib/deck.rb
@@ -1,13 +1,16 @@
 class Deck
-  attr_reader :cards, :count
+  attr_reader :cards
   
-  def initialize(cards = [])
+  def initialize(cards)
     @cards = cards
-    @count = cards.count
+  end
+
+  def count
+    cards.count
   end
 
   def cards_in_category(category)
     @cards.select{|card| card.category == category}
   end
-  
+
 end
