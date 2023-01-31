69ee76baa4613d5c0b01097e1a93967ce58cbc80
Katie Lonsdale <katie952@gmail.com>
2023-01-31 13:19:11 -0700

    add deck tests

---
+++ flash_cards/spec/deck_spec.rb
@@ -0,0 +1,45 @@
+require './lib/deck'
+require './lib/card'
+
+RSpec.describe Deck do
+  it 'exists' do
+    deck = Deck.new
+
+    expect(deck).to be_instance_of(Deck)
+  end
+
+  it 'takes an array of cards' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    test_cards = [card_1, card_2, card_3]
+    deck = Deck.new(test_cards)
+
+    expect(deck.cards).to be(test_cards)
+  end
+
+  it 'returns number of cards in deck' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    expect(deck.count).to eq 3
+  end
+
+  xit 'returns cards in given category' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    expect(deck.cards_in_category(:STEM)).to include(card_2)
+    expect(deck.cards_in_category(:STEM)).to include(card_3)
+    expect(deck.cards_in_category(:STEM)).not_to include(card_1)
+  end
+end
