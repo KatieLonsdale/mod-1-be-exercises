b9e2dbe8ff4110230dc0e5d920503a7edca59523
Katie Lonsdale <katie952@gmail.com>
2023-02-01 14:45:10 -0700

    add game over stats

--- flash_cards/flashcard_runner.rb
+++ flash_cards/flashcard_runner.rb
@@ -21,22 +21,29 @@ puts "This is card number 1 out of 4."
 puts "Question: #{card_1.question}"
 guess_1 = gets.chomp
 turn_1 = Turn.new(guess_1,card_1)
+round_1.take_turn(guess_1)
 puts turn_1.feedback
 
 puts "This is card number 2 out of 4."
 puts "Question: #{card_2.question}"
 guess_2 = gets.chomp
 turn_2 = Turn.new(guess_2,card_2)
+round_1.take_turn(guess_2)
 puts turn_2.feedback
 
 puts "This is card number 3 out of 4."
 puts "Question: #{card_3.question}"
 guess_3 = gets.chomp
 turn_3 = Turn.new(guess_3,card_3)
+round_1.take_turn(guess_3)
 puts turn_3.feedback
 
 puts "This is card number 4 out of 4."
 puts "Question: #{card_4.question}"
 guess_4 = gets.chomp
 turn_4 = Turn.new(guess_4,card_4)
+round_1.take_turn(guess_4)
 puts turn_4.feedback
+
+puts '****** Game over! ******'
+puts "You had #{round_1.number_correct} correct guesses out of #{round_1.turns.count} for a total score of #{round_1.percent_correct}%."
