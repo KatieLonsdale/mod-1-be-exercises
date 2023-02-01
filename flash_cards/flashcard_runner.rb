ac296e4fa3ff312c1b9ad2135549b85cdfee652b
Katie Lonsdale <katie952@gmail.com>
2023-02-01 14:36:48 -0700

    add turn 2

--- flash_cards/flashcard_runner.rb
+++ flash_cards/flashcard_runner.rb
@@ -16,8 +16,16 @@ round_1 = Round.new(deck_1)
 
 puts "Welcome! You're playing with 4 cards."
 puts '-------------------------------------------------'
+
 puts "This is card number 1 out of 4."
 puts "Question: #{card_1.question}"
 guess_1 = gets.chomp
 turn_1 = Turn.new(guess_1,card_1)
 puts turn_1.feedback
+
+puts "This is card number 2 out of 4."
+puts "Question: #{card_2.question}"
+guess_2 = gets.chomp
+turn_2 = Turn.new(guess_2,card_2)
+puts turn_2.feedback
+
