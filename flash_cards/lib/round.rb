09c38ae143d240a53394889cce03f1e93a68355d
Katie Lonsdale <katie952@gmail.com>
2023-01-31 19:53:49 -0700

    refactor number correct methods

--- flash_cards/lib/round.rb
+++ flash_cards/lib/round.rb
@@ -1,10 +1,9 @@
 class Round
-  attr_reader :deck, :turns, :number_correct
+  attr_reader :deck, :turns
 
   def initialize(deck)
     @deck = deck
     @turns = []
-    @number_correct = 0
   end
 
   def current_card
@@ -14,12 +13,29 @@ class Round
   def take_turn(guess)
     current_turn = Turn.new(guess, self.current_card)
     turns.push(current_turn)
-    @number_correct += 1 if current_turn.correct? == true
     current_turn
   end
 
+  def number_correct
+    number_right = 0
+    @turns.each do |turn|
+      if turn.correct? == true
+      number_right += 1
+      else
+      end
+    end
+    number_right
+  end
+
   def percent_correct
-    (@number_correct.to_f) / (@turns.count.to_f) * 100
+    number_right = 0
+    @turns.each do |turn|
+      if turn.correct? == true
+        number_right += 1
+      else
+      end
+    end
+    (number_right.to_f) / (@turns.count.to_f) * 100
   end
 
   def number_correct_by_category(category)
