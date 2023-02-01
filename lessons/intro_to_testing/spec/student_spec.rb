59fc580c6d69cf7e7e834145dbf3010f87dd5afb
Katie Lonsdale <katie952@gmail.com>
2023-02-01 15:30:40 -0700

    update lesson files

--- lessons/intro_to_testing/spec/student_spec.rb
+++ lessons/intro_to_testing/spec/student_spec.rb
@@ -1,13 +1,32 @@
 # Add the 2 requires you will need here
+require 'rspec'
+require './lib/student'
 
-
-describe Student do
+RSpec.describe Student do
   describe '#initialize' do
     it 'is an instance of student' do
-      # write the code to initialize a new student object
+      student = Student.new('Penelope')
+
+      expect(student).to be_instance_of(Student)
     end
-    # test it has a name
-    # test it has cookies
-    # test it can add cookies
+
+    it 'has attributes' do
+      student = Student.new('Penelope')
+
+      expect(student.name).to eq('Penelope')
+      expect(student.cookies).to eq([])
+    end
   end
+
+  describe '#add_cookie' do
+    it 'adds a cookie to the student cookies array' do
+      student = Student.new('Penelope')
+    
+      student.add_cookie('Chocolate Chunk')
+      student.add_cookie('Snickerdoodle')
+
+      expect(student.cookies).to eq(['Chocolate Chunk', 'Snickerdoodle'])
+    end
+  end
+
 end
