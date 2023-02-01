d0f69109472c1f6b8d79b73186b7884866fe753e
Katie Lonsdale <katie952@gmail.com>
2023-01-31 19:26:46 -0700

    add percent correct method

--- flash_cards/lib/round.rb
+++ flash_cards/lib/round.rb
@@ -1,9 +1,10 @@
 class Round
-  attr_reader :deck, :turns
+  attr_reader :deck, :turns, :number_correct
 
   def initialize(deck)
     @deck = deck
     @turns = []
+    @number_correct = 0
   end
 
   def current_card
@@ -13,7 +14,12 @@ class Round
   def take_turn(guess)
     current_turn = Turn.new(guess, self.current_card)
     turns.push(current_turn)
+    @number_correct += 1 if current_turn.correct? == true
     current_turn
+  end
+
+  def percent_correct
+    (@number_correct.to_f) / (@turns.count.to_f) * 100
   end
 
 end
