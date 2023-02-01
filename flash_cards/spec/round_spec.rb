0afe7e6770b254ba3103e54e7e98f8d52d72a33a
Katie Lonsdale <katie952@gmail.com>
2023-01-31 18:09:45 -0700

    fix testing typo

--- flash_cards/spec/round_spec.rb
+++ flash_cards/spec/round_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe do
     expect(round.deck).to eq(deck)
   end
 
-  xit 'has no turns be default' do
+  it 'has no turns be default' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -45,7 +45,7 @@ RSpec.describe do
     expect(round.current_card).to eq(card_3)
   end
 
-  xit 'creates new turn each time you take a turn' do
+  it 'creates new turn each time you take a turn' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -54,7 +54,7 @@ RSpec.describe do
 
     new_turn = round.take_turn('Juneau')
 
-    expect(new_turn.class).to eq 'turn'
+    expect(new_turn.class).to eq Turn
   end
 
   xit 'evaluates answer' do
