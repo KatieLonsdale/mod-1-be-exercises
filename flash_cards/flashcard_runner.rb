023ebeb24124de72df6a080ba7a9d107dc062939
Katie Lonsdale <katie952@gmail.com>
2023-02-02 15:51:19 -0700

    add card generator in runner

--- flash_cards/flashcard_runner.rb
+++ flash_cards/flashcard_runner.rb
@@ -2,51 +2,50 @@ require './lib/card'
 require './lib/deck'
 require './lib/round'
 require './lib/turn'
+require './lib/card_generator'
+require 'rspec'
 
 def start
-  card_1 = Card.new('What is the capital of Colorado?', 'Denver', :Geography)
-  card_2 = Card.new('What is 10 x 10?', '100', :Math)
-  card_3 = Card.new('How many states make up New England?', '6', :Geography)
-  card_4 = Card.new('What is the power house of the cell?', 'The mitochondria', :Biology)
+  filename = "spec/cards.txt"
+  cards = CardGenerator.new(filename)
 
-  cards = [card_1, card_2, card_3, card_4]
-
-  deck_1 = Deck.new(cards)
+  deck_1 = Deck.new(cards.cards)
 
   round_1 = Round.new(deck_1)
 
-  puts "Welcome! You're playing with #{cards.count} cards."
+  puts "Welcome! You're playing with #{deck_1.count} cards."
   puts '-------------------------------------------------'
 
-  puts "This is card number #{round_1.turn_number} out of #{cards.count}."
+  puts "This is card number #{round_1.turn_number} out of #{deck_1.count}."
   puts "Question: #{round_1.current_card.question}"
   guess_1 = gets.chomp
-  turn_1 = Turn.new(guess_1,card_1)
+  turn_1 = Turn.new(guess_1,deck_1.cards[0])
   round_1.take_turn(guess_1)
   puts turn_1.feedback
 
-  puts "This is card number #{round_1.turn_number} out of #{cards.count}."
+  puts "This is card number #{round_1.turn_number} out of #{deck_1.count}."
   puts "Question: #{round_1.current_card.question}"
   guess_2 = gets.chomp
-  turn_2 = Turn.new(guess_2,card_2)
+  turn_2 = Turn.new(guess_2,deck_1.cards[1])
   round_1.take_turn(guess_2)
   puts turn_2.feedback
 
-  puts "This is card number #{round_1.turn_number} out of #{cards.count}."
+  puts "This is card number #{round_1.turn_number} out of #{deck_1.count}."
   puts "Question: #{round_1.current_card.question}"
   guess_3 = gets.chomp
-  turn_3 = Turn.new(guess_3,card_3)
+  turn_3 = Turn.new(guess_3,deck_1.cards[2])
   round_1.take_turn(guess_3)
   puts turn_3.feedback
 
-  puts "This is card number #{round_1.turn_number} out of #{cards.count}."
+  puts "This is card number #{round_1.turn_number} out of #{deck_1.count}."
   puts "Question: #{round_1.current_card.question}"
   guess_4 = gets.chomp
-  turn_4 = Turn.new(guess_4,card_4)
+  turn_4 = Turn.new(guess_4,deck_1.cards[3])
   round_1.take_turn(guess_4)
   puts turn_4.feedback
 
   puts '****** Game over! ******'
+  # need to figure out how to fix categories
   puts "You had #{round_1.number_correct} correct guesses out of #{round_1.turns.count} for a total score of #{round_1.percent_correct}%."
   puts "Geography - #{round_1.percent_correct_by_category(:Geography)}% correct"
   puts "Math - #{round_1.percent_correct_by_category(:Math)}% correct"
