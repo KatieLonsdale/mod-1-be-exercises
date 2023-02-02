8fdd23db89f85d37517737e3abe935b1a3ceef5e
Katie Lonsdale <katie952@gmail.com>
2023-02-01 18:57:29 -0700

    fix feedback test

--- flash_cards/spec/turn_spec.rb
+++ flash_cards/spec/turn_spec.rb
@@ -45,6 +45,6 @@ RSpec.describe Turn do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new('Juneau', card)
 
-    expect(turn.feedback).to eq('Correct!')
+    expect(turn.feedback).to eq('Correct.')
   end
 end
