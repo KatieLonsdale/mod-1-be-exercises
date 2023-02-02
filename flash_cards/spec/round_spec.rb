54094b9a667b46ac11ecfae9a5af0544fff29859
Katie Lonsdale <katie952@gmail.com>
2023-02-01 18:55:32 -0700

    fix tests

--- flash_cards/spec/round_spec.rb
+++ flash_cards/spec/round_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe do
       deck = Deck.new([card_1, card_2, card_3])
       round = Round.new(deck)
 
-      expect(round.turn_number).to eq 0
+      expect(round.turn_number).to eq 1
       expect(round.turns).to eq []
     end
   end
@@ -40,6 +40,7 @@ RSpec.describe do
       new_turn = round.take_turn('Juneau')
 
       expect(new_turn.class).to eq Turn
+      expect(round.turns).to eq([new_turn])
     end
 
     it 'tracks current card' do
@@ -61,7 +62,7 @@ RSpec.describe do
     end
 
     
-    it 'keeps track of number of turns' do
+    it 'tracks number of turns' do
       card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
       card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
       card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -71,12 +72,12 @@ RSpec.describe do
       new_turn = round.take_turn('Juneau')
       
       expect(round.turns).to eq [new_turn]
-      expect(round.turn_number).to eq 1
+      expect(round.turn_number).to eq 2
       
       new_turn_2 = round.take_turn('Venus')
       
       expect(round.turns).to eq [new_turn, new_turn_2]
-      expect(round.turn_number).to eq 2
+      expect(round.turn_number).to eq 3
     end
   end
 
