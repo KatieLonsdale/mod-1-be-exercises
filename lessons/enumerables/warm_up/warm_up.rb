15052c1a82649443852fd2b6bae2b08f4d1780d2
Katie Lonsdale <katie952@gmail.com>
2023-02-08 20:05:50 -0700

    update with classwork

--- lessons/enumerables/warm_up/warm_up.rb
+++ lessons/enumerables/warm_up/warm_up.rb
@@ -2,9 +2,14 @@
 # and returns an array of all names upcased.
 
 kardashians = ["kris", "kim", "kourtney", "khloe", "rob"]
+# map
 
 def upcase_names(names)
- # your code here
+  upcase_kardashians = []
+  names.each do |name|
+    upcase_kardashians << name.upcase
+  end
+  upcase_kardashians
 end 
 
 p upcase_names(kardashians)
@@ -20,9 +25,14 @@ p upcase_names(kardashians)
 # and returns a collection of the names that end in 'ie'
 
 pets = ["Brutus", "Lucky", "Goldie", "Pepper", "Odie"]
+# find all
 
 def names_ending_in_ie(names)
- # your code here
+ ie_pets = []
+ names.each do |name|
+  ie_pets << name if name.end_with?('ie')
+ end
+ ie_pets
 end 
 
 p names_ending_in_ie(pets)
@@ -32,15 +42,21 @@ p names_ending_in_ie(pets)
 
 
 
-
-
 # 3. Using #each, update the method below, such that it takes in an array of integers,
 # and returns the first number that is over 100 when squared. 
 
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+# find
 
 def first_num_thats_square_is_above_100(nums)
-
+  integer = 0
+  nums.each do |num|
+    until integer ** 2 > 100
+      integer = num
+      break
+    end
+  end
+  integer
 end 
 
 p first_num_thats_square_is_above_100(nums)
