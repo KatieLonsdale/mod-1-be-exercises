a849c2a3a1dce1d817ade0f2829178c61f4f11fc
Katie Lonsdale <katie952@gmail.com>
2023-02-08 20:04:57 -0700

    add menu and ration methods

--- practice_independent_challenges/week2/potluck/lib/potluck.rb
+++ practice_independent_challenges/week2/potluck/lib/potluck.rb
@@ -16,4 +16,26 @@ class Potluck
     end
   end
 
+  def menu
+    menu_by_course = {}
+    @dishes.each do |dish|
+      course = dish.category.to_s.concat('s').to_sym
+      if menu_by_course.has_key?(course)
+        menu_by_course[course].push(dish.name)
+        menu_by_course[course].sort!
+      else
+        menu_by_course[course] = []
+        menu_by_course[course].push(dish.name)
+      end
+    end
+    menu_by_course
+  end
+
+  def ratio(category)
+    count_of_category = @dishes.find_all.count do |dish|
+      dish.category == category
+    end
+    (count_of_category.to_f / dishes.count.to_f) * 100
+  end
+
 end
--- practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
@@ -56,4 +56,47 @@ RSpec.describe do
       expect(potluck.get_all_from_category(:appetizer).first.name).to eq('Couscous Salad')
     end
   end
+
+  describe '#menu' do
+    it 'returns dishes in order of course and alphabetically' do
+      potluck = Potluck.new("7-13-18")
+      couscous_salad = Dish.new("Couscous Salad", :appetizer)
+      summer_pizza = Dish.new("Summer Pizza", :appetizer)
+      roast_pork = Dish.new("Roast Pork", :entree)
+      cocktail_meatballs = Dish.new("Cocktail Meatballs", :entree)
+      candy_salad = Dish.new("Candy Salad", :dessert)
+      bean_dip = Dish.new('Bean Dip', :appetizer)
+
+      potluck.add_dish(couscous_salad)
+      potluck.add_dish(summer_pizza)
+      potluck.add_dish(roast_pork)
+      potluck.add_dish(cocktail_meatballs)
+      potluck.add_dish(candy_salad)
+      potluck.add_dish(bean_dip)
+
+      expect(potluck.menu).to eq({:appetizers=>["Bean Dip", "Couscous Salad", "Summer Pizza"],:entrees=>["Cocktail Meatballs", "Roast Pork"],:desserts=>["Candy Salad"]})
+    end
+
+  end
+
+  describe '#ratio' do
+    it 'returns percentage of total meals for given category' do
+      potluck = Potluck.new("7-13-18")
+      couscous_salad = Dish.new("Couscous Salad", :appetizer)
+      summer_pizza = Dish.new("Summer Pizza", :appetizer)
+      roast_pork = Dish.new("Roast Pork", :entree)
+      cocktail_meatballs = Dish.new("Cocktail Meatballs", :entree)
+      candy_salad = Dish.new("Candy Salad", :dessert)
+      bean_dip = Dish.new('Bean Dip', :appetizer)
+
+      potluck.add_dish(couscous_salad)
+      potluck.add_dish(summer_pizza)
+      potluck.add_dish(roast_pork)
+      potluck.add_dish(cocktail_meatballs)
+      potluck.add_dish(candy_salad)
+      potluck.add_dish(bean_dip)
+
+      expect(potluck.ratio(:appetizer)).to eq 50.0
+    end
+  end
 end
