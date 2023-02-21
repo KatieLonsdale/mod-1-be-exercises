a97b00d3610291cf182854028c26f78020a796e6
Katie Lonsdale <katie952@gmail.com>
2023-02-21 10:12:09 -0700

    update files

--- lessons/inheritance/lesson_example/spec/ceo_spec.rb
+++ lessons/inheritance/lesson_example/spec/ceo_spec.rb
@@ -1,19 +1,35 @@
 require './lib/ceo'
+require './lib/employee'
+
 
 RSpec.describe Ceo do
 
   describe "setup" do
     it "exists" do
-      ceo = Ceo.new(15,20)
+      ceo = Ceo.new(15,20,"Steve", 1234)
     end
     it "has base_salary and bonus attributes" do
-      ceo = Ceo.new(15,20)
+      ceo = Ceo.new(15,20,"Steve", 1234)
       expect(ceo.base_salary).to eq(15)
       expect(ceo.bonus).to eq(20)
     end
   end
 
   describe "Has a name, ID, and total_compensation" do
+    it 'has name and id number and total compensation' do
+      ceo = Ceo.new(15,20,"Steve", 1234)
+      expect(ceo.name).to eq('Steve')
+      expect(ceo.id).to eq(1234)
+      expect(ceo.total_compensation).to eq(35)
+    end
+
+    it 'is an Employee and CEO' do
+      ceo = Ceo.new(15,20,"Steve", 1234)
+      
+      expect(ceo.is_a? Ceo).to be true
+      expect(ceo.is_a? Employee).to be true
+    end
+
     # Write tests to prove that a Ceo instance can have a Name and ID number, like any other Employee.
     # Also, the CEO should be able to receive a total compensation. Write at least one test that proves they can access this method.
   end
--- lessons/inheritance/lesson_example/spec/manager_spec.rb
+++ lessons/inheritance/lesson_example/spec/manager_spec.rb
@@ -3,12 +3,12 @@ require "./lib/manager"
 RSpec.describe Manager do
   describe "setup" do
     it "exists" do
-      manager = Manager.new(12)
+      manager = Manager.new(12, 'Bob', 8990)
       expect(manager).to be_a Manager
     end
 
     it "has base_salary, per_team_rate, and departments attributes" do
-      manager = Manager.new(12)
+      manager = Manager.new(12, 'Bob', 8990)
       expect(manager.base_salary).to eq(12)
       expect(manager.per_team_rate).to eq 5
       expect(manager.departments).to be_instance_of Array
@@ -16,20 +16,41 @@ RSpec.describe Manager do
     end
 
     it "can add_department" do
-      manager = Manager.new(12)
+      manager = Manager.new(12, 'Bob', 8990)
       manager.add_department("Sales")
       expect(manager.departments).to eq(["Sales"])
       expect(manager.departments.length).to eq(1)
     end
 
     it "has benefits to start with" do
-      manager = Manager.new(12)
+      manager = Manager.new(12, 'Bob', 8990)
       expect(manager.benefits).to eq([:sick_leave, :health_insurance])
     end
   end
 
   describe "Ability to have name and ID, like any other Employee" do
+      it 'has a name and id number' do
+        manager = Manager.new(12, 'Bob', 8990)
+
+        expect(manager.name).to eq('Bob')
+        expect(manager.id).to eq(8990)
+      end
+
+      it 'is an Employee and a Manager' do
+        manager = Manager.new(12, 'Bob', 8990)
+        
+        expect(manager.is_a? Manager).to be true
+        expect(manager.is_a? Employee).to be true
+      end
     # Write tests to prove that a Manager instance can have a Name and ID number, like any other Employee.
     # Also, a Manager should be able to receive a total compensation. Write at least one test that proves they can access this method.
+  end
+
+  describe 'total compensation' do
+    it 'is able to receive total compensation' do
+      manager = Manager.new(12, 'Bob', 8990)
+
+      expect(manager.total_compensation).to eq(72)
+    end
   end
 end
