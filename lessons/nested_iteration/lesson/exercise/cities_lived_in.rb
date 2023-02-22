5eb0c1e1a780e0eaae2b95abe2dc308eeeee389d
Katie Lonsdale <katie952@gmail.com>
2023-02-22 10:58:39 -0700

    update file

--- lessons/nested_iteration/lesson/exercise/cities_lived_in.rb
+++ lessons/nested_iteration/lesson/exercise/cities_lived_in.rb
@@ -1,14 +1,20 @@
-cities_lived_in = {
-    michaela: ["Philadelphia", "Fort Collins", "Seattle"],
-    mike: ["Denver", "Santa Fe", "Philadelphia", "Portland"],
-    pamela: ["Lansing"],
-    alex: ["Seattle", "Columbus", "Philadelphia", "Austin"]
-}
+
 
 
 # Problem #1: 
 # Get a unique list of all of the cities that these humans have lived in  
 # ["Philadelphia", "Fort Collins", "Seattle", "Denver", "Santa Fe", "Portland", "Lansing", "Columbus", "Austin"]
+
+# cities = []
+# cities_lived_in.each_value do |value|
+#     value.each do |city|
+#         unless cities.include?(city)
+#             cities << city
+#         end
+#     end
+# end
+
+# p cities
 
 
 
@@ -21,8 +27,35 @@ cities_lived_in = {
 # or
 # ["Michaela", "Mike", "Salvador"]
 
+# people = []
+
+# cities_lived_in.each do |name, cities|
+#     if cities.include?('Philadelphia')
+#         people << name
+#     end
+# end
+
+# p people
 
 
+
+cities_lived_in = {
+    michaela: ["Philadelphia", "Fort Collins", "Seattle"],
+    mike: ["Denver", "Santa Fe", "Philadelphia", "Portland"],
+    pamela: ["Lansing"],
+    alex: ["Seattle", "Columbus", "Philadelphia", "Austin"]
+}
+
+hash = {}
+cities = []
+cities_lived_in.each_value do |value|
+    value.each do |city|
+        cities << city
+        hash[city] = cities.count(city)
+    end
+end
+
+p hash
 
 # Problem #3: 
 # Create a hash that has the city as a key, and the number of people that live in it as it's value: 
