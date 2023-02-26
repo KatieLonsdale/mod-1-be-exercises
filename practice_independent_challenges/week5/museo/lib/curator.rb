269d6c7cf7403e03e013c619149d81e3bcfed8ea
Katie Lonsdale <katie952@gmail.com>
2023-02-26 14:28:08 -0700

    feat: add photos by country method

--- practice_independent_challenges/week5/museo/lib/curator.rb
+++ practice_independent_challenges/week5/museo/lib/curator.rb
@@ -22,4 +22,15 @@ class Curator
     collection = @photographs.group_by{|photo| photo.artist_id}
     collection.transform_keys{|id| find_artist_by_id(id)}
   end
+
+  def artists_with_multiple_photographs
+    collection.select{|artist, photos| artist if photos.count > 1}.keys
+  end
+
+  def photos_by_country(country)
+    photos_array = collection.select do |artist, photos| 
+      photos if artist.country == country
+    end
+    photos_array.empty? ? 'No artist found.' : photos_array.values.flatten
+  end
 end
