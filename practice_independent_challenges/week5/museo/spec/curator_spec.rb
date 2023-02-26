147b0812b6125a914832ada0f1205cda0ceaef7b
Katie Lonsdale <katie952@gmail.com>
2023-02-26 13:11:18 -0700

    test: add add artist test

--- practice_independent_challenges/week5/museo/spec/curator_spec.rb
+++ practice_independent_challenges/week5/museo/spec/curator_spec.rb
@@ -15,6 +15,20 @@ RSpec.describe Curator do
      artist_id: "2",      
      year: "1941"      
     })
+    @artist_1 = Artist.new({
+    id: "1",      
+    name: "Henri Cartier-Bresson",      
+    born: "1908",      
+    died: "2004",      
+    country: "France"      
+    })        
+    @artist_2 = Artist.new({
+    id: "2",      
+    name: "Ansel Adams",      
+    born: "1902",      
+    died: "1984",      
+    country: "United States"      
+    })        
   end
   describe '#initialize' do
     it 'exists' do
@@ -24,6 +38,11 @@ RSpec.describe Curator do
       expect(@curator.photographs).to eq([])
       expect(@curator.photographs).to be_a Array
     end
+    it 'has an empty array of artists by default' do
+      expect(@curator.artists).to eq([])
+      expect(@curator.artists).to be_a Array
+
+    end
   end
   describe '#add_photograph' do
     it 'adds given photo to photographs array' do
@@ -32,6 +51,17 @@ RSpec.describe Curator do
 
       expect(@curator.photographs).to eq([@photo_1, @photo_2])
       expect(@curator.photographs.count).to eq 2
+    end
+  end
+
+  describe '#add_artist' do
+    it 'adds an artist to artists array' do
+      @curator.add_artist(@artist_1)
+      @curator.add_artist(@artist_2)
+
+      expect(@curator.artists).to eq([@artist_1, @artist_2])
+      expect(@curator.artists.count).to eq 2
+
     end
   end
 end
