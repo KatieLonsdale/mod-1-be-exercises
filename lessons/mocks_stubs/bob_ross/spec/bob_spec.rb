dc601687605dae21b6ad780d964b2d97a075a542
Katie Lonsdale <katie952@gmail.com>
2023-03-01 09:49:21 -0700

    updating changes from class

--- lessons/mocks_stubs/bob_ross/spec/bob_spec.rb
+++ lessons/mocks_stubs/bob_ross/spec/bob_spec.rb
@@ -20,8 +20,9 @@ RSpec.describe Bob do
   describe 'Methods' do
     it 'can add paint' do
       bob = Bob.new
-      paint_1 = Paint.new("Alizarin Crimson")
-      paint_2 = Paint.new("Van Dyke Brown")
+      paint_1 = double("paint 1")
+      paint_2 = double()
+      # mock for paint
 
       bob.add_paint(paint_1)
       bob.add_paint(paint_2)
@@ -31,10 +32,15 @@ RSpec.describe Bob do
 
     it 'can return paint colors' do
       bob = Bob.new
-      paint_1 = Paint.new("Alizarin Crimson")
-      paint_2 = Paint.new("Van Dyke Brown")
+      paint_1 = double("paint 1")
+      paint_2 = double()
+
       bob.add_paint(paint_1)
       bob.add_paint(paint_2)
+
+      allow(paint_1).to receive(:color).and_return('Alizarin Crimson')
+      allow(paint_2).to receive(:color).and_return('Van Dyke Brown')
+      # stubs
 
       expect(bob.paint_colors).to eq(["Alizarin Crimson", "Van Dyke Brown"])
     end
