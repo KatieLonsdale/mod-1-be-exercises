ffa30bf8da728b030f935a2cf1ea3a725abd9441
Katie Lonsdale <katie952@gmail.com>
2023-03-02 14:22:24 -0700

    add mocks tests

--- lessons/mocks_stubs/user_image_generator/spec/image_generator_spec.rb
+++ lessons/mocks_stubs/user_image_generator/spec/image_generator_spec.rb
@@ -16,4 +16,23 @@ RSpec.describe ImageGenerator do
     # Using stubs write the tests to confirm the functionality
     # that exists in ./lib/image_generator.rb
   end
+
+  describe '#generate_images' do
+    it 'generates images' do
+      image_generator = ImageGenerator.new(2500)
+      allow(image_generator).to receive(:rand).and_return(100)
+
+      expect(image_generator.generate_images.count).to eq 100
+    end
+  end
+
+  describe '#change_max_size' do
+    it 'changes the max image size' do
+      image_generator = ImageGenerator.new(2500)
+      allow(image_generator).to receive(:rand).and_return(500)
+      image_generator.change_max_size(2)
+
+      expect(image_generator.max_image_size).to eq 1000
+    end
+  end
 end
