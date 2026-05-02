868a914b326c3b2cdf98778a0ac030772d07b801
Katie Lonsdale <katie952@gmail.com>
2026-05-02 12:18:57 -0600

    Add age_at_death and test

--- practice_independent_challenges/week5/museo/spec/artist_spec.rb
+++ practice_independent_challenges/week5/museo/spec/artist_spec.rb
@@ -1,5 +1,5 @@
 require 'rspec'
-require './lib/artist'
+require_relative '../lib/artist.rb'
 
 RSpec.describe 'Artist' do
   it 'exists and has the correct attributes' do
@@ -18,5 +18,22 @@ RSpec.describe 'Artist' do
     expect(artist_1.born).to eq("1902")
     expect(artist_1.died).to eq("1984")
     expect(artist_1.country).to eq("United States")
+  end
+
+  describe 'instance methods' do
+    describe '#age_at_death' do
+      it 'can calculate age at death' do
+        attributes = {
+          id: "2",
+          name: "Ansel Adams",
+          born: "1902",
+          died: "1984",
+          country: "United States"
+        }
+        artist_1 = Artist.new(attributes)
+
+        expect(artist_1.age_at_death).to eq(82)
+      end
+    end
   end
 end
