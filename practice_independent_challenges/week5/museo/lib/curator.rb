122587894dd53632457b723c81f2ff92c579c0fa
Katie Lonsdale <katie952@gmail.com>
2026-05-03 13:15:00 -0600

    Add artists_with_photographs and prolific_artist_names methods and tests

--- practice_independent_challenges/week5/museo/lib/curator.rb
+++ practice_independent_challenges/week5/museo/lib/curator.rb
@@ -17,7 +17,18 @@ class Curator
   def find_artist_by_id(id)
     @artists.find { |artist| artist.id == id }
   end
+
+  def artists_with_photographs
+    @artists.each_with_object({}) do |artist, hash|
+      hash[artist] = @photographs.select { |photo| photo.artist_id == artist.id }
+    end
+  end
+
+  def prolific_artist_names
+    artists_with_photographs.select { |artist, photos| photos.length > 1 }.map { |artist, photos| artist.name }
+  end
 end
+
 
 
 
--- practice_independent_challenges/week5/museo/spec/curator_spec.rb
+++ practice_independent_challenges/week5/museo/spec/curator_spec.rb
@@ -97,6 +97,148 @@ RSpec.describe 'Curator' do
         expect(@curator.find_artist_by_id("5")).to eq(nil)
       end
     end
+
+    describe '#artists_with_photographs' do
+      it 'returns artists and their photographs' do
+        artist_1 = Artist.new({
+          id: "1",
+          name: "Henri Cartier-Bresson",
+          born: "1908",
+          died: "2004",
+          country: "France"
+        })
+
+        artist_2 = Artist.new({
+          id: "2",
+          name: "Ansel Adams",
+          born: "1902",
+          died: "1984",
+          country: "United States"
+        })
+
+        artist_3 = Artist.new({
+          id: "3",
+          name: "Diane Arbus",
+          born: "1923",
+          died: "1971",
+          country: "United States"
+        })
+
+        photograph_1 = Photograph.new({
+          id: "1",
+          name: "Rue Mouffetard, Paris (Boy with Bottles)",
+          artist_id: "1",
+          year: "1954"
+        })
+
+        photograph_2 = Photograph.new({
+          id: "2",
+          name: "Moonrise, Hernandez",
+          artist_id: "2",
+          year: "1941"
+        })
+
+        photograph_3 = Photograph.new({
+          id: "3",
+          name: "Behind the Gare Saint-Lazare",
+          artist_id: "1",
+          year: "1932"
+        })
+
+        curator = Curator.new
+        curator.add_artist(artist_1)
+        curator.add_artist(artist_2)
+        curator.add_artist(artist_3)
+        curator.add_photograph(photograph_1)
+        curator.add_photograph(photograph_2)
+        curator.add_photograph(photograph_3)
+
+        result = curator.artists_with_photographs
+
+        expect(result).to be_a(Hash)
+        expect(result[artist_1]).to eq([photograph_1, photograph_3])
+        expect(result[artist_2]).to eq([photograph_2])
+        expect(result[artist_3]).to eq([])
+      end
+    end
+
+    describe '#prolific_artist_names' do
+      it 'returns names of artists with more than one photograph' do
+        artist_1 = Artist.new({
+          id: "1",
+          name: "Henri Cartier-Bresson",
+          born: "1908",
+          died: "2004",
+          country: "France"
+        })
+
+        artist_2 = Artist.new({
+          id: "2",
+          name: "Ansel Adams",
+          born: "1902",
+          died: "1984",
+          country: "United States"
+        })
+
+        artist_3 = Artist.new({
+          id: "3",
+          name: "Diane Arbus",
+          born: "1923",
+          died: "1971",
+          country: "United States"
+        })
+
+        photograph_1 = Photograph.new({
+          id: "1",
+          name: "Rue Mouffetard, Paris (Boy with Bottles)",
+          artist_id: "1",
+          year: "1954"
+        })
+
+        photograph_2 = Photograph.new({
+          id: "2",
+          name: "Moonrise, Hernandez",
+          artist_id: "2",
+          year: "1941"
+        })
+
+        photograph_3 = Photograph.new({
+          id: "3",
+          name: "Behind the Gare Saint-Lazare",
+          artist_id: "1",
+          year: "1932"
+        })
+
+        photograph_4 = Photograph.new({
+          id: "4",
+          name: "Child with Toy Hand Grenade",
+          artist_id: "3",
+          year: "1962"
+        })
+
+        photograph_5 = Photograph.new({
+          id: "5",
+          name: "Identical Twins",
+          artist_id: "3",
+          year: "1967"
+        })
+
+        curator = Curator.new
+        curator.add_artist(artist_1)
+        curator.add_artist(artist_2)
+        curator.add_artist(artist_3)
+        curator.add_photograph(photograph_1)
+        curator.add_photograph(photograph_2)
+        curator.add_photograph(photograph_3)
+        curator.add_photograph(photograph_4)
+        curator.add_photograph(photograph_5)
+
+        result = curator.prolific_artist_names
+
+        expect(result).to be_a(Array)
+        expect(result).to eq(["Henri Cartier-Bresson", "Diane Arbus"])
+      end
+    end
   end
 end
 
